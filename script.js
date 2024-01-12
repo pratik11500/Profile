72814eab6ce51546549ce67c1ab185dd2de2e886
Pratik <139073467+pratik11500@users.noreply.github.com>
2024-01-13 03:46:57 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+$(window).on('scroll', function() {
+  if ($(window).scrollTop()) {
+    $('header').addClass('nav-show');
+  } else {
+    $('header').removeClass('nav-show');
+  }
+})
+const navSlide = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".nav-bar");
+  const navLinks = document.querySelectorAll(".nav-bar li");
+  hamburger.onclick = () => {
+    navbar.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+      }
+    }
+    );
+    hamburger.classList.toggle("toggle");
+  }
+}
+window.onload = () => navSlide();
+const updateVisitorCount = async () => {
+  try {
+    const response = await fetch('#');
+    const result = await response.json();
+    document.getElementById('visitor-count').innerHTML = result.value;
+  } catch (error) {
+    console.error(error);
+  }
+}
+  ;
+updateVisitorCount();
+
+vvvvvvvvvvvvvvvvvvvvv
