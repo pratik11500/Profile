7b64f3b8cd23fc3b6c1c9de8ab0fc93e248e4421
Pratik <139073467+pratik11500@users.noreply.github.com>
2024-01-15 12:55:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,34 @@ const updateVisitorCount = async () => {
   } catch (error) {
     console.error(error);
   }
+
+  var blockContextMenu = function (e) {
+    // Prevent right-click context menu
+    e.preventDefault();
+};
+
+  
+
+var showWarning = function () {
+    var warningPopup = document.createElement('div');
+    warningPopup.innerHTML = '<p>Content is Protected!</p>';
+    warningPopup.classList.add('warning-popup');
+    document.body.appendChild(warningPopup);
+
+    // Remove the warning after a delay (e.g., 2 seconds)
+    setTimeout(function () {
+        document.body.removeChild(warningPopup);
+    }, 2000);
+};
+
+document.addEventListener('contextmenu', blockContextMenu);
+document.addEventListener('keydown', function (e) {
+    // Disable F12 key
+    if (e.key === 'F12' || e.keyCode === 123) {
+        e.preventDefault();
+        showWarning();
+    }
+});
 }
   ;
 updateVisitorCount();
